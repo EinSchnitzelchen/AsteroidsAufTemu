50a1d1b5a4fb3921ca2adcf693157e5817694d35
EinSchnitzelchen <76046095+EinSchnitzelchen@users.noreply.github.com>
2025-02-11 20:33:19 +0100

    Player Movement and shooting implemented

--- Julien/PYgame/Game.py
+++ Julien/PYgame/Game.py
@@ -6,11 +6,9 @@ class Game:
 
     def __init__(self):
         self.run = True
-        self.screen_width = 1060
-        self.screen_height = 798
-        WHITE = (255, 255, 255)
-        self.image = pygame.Surface((50, 50)) 
-        self.image.fill(WHITE)
+        self.screen_width = 1920
+        self.screen_height = 1050
+        self.image = pygame.image.load("Julien/PYgame/Sprites/background/background.png") 
         self.image = pygame.transform.scale(self.image, (self.screen_width, self.screen_height))
         self.screen = pygame.display.set_mode((self.screen_width, self.screen_height))
 
@@ -18,23 +16,23 @@ class Game:
 
         self.bullet_group = pygame.sprite.Group()
 
-        self.tank = Tank()
-        self.all_sprites.add(self.tank)
+        self.spaceship = Spaceship()
+        self.all_sprites.add(self.spaceship)
 
-        bullet = Bullet(self.tank)
+        bullet = Bullet(self.spaceship)
         self.bullet_group.add(bullet)
         self.all_sprites.add(bullet)
 
 
     def handle_events(self):
         keys = pygame.key.get_pressed()
-        self.tank.handle_events()
+        self.spaceship.handle_events()
 
 
         if keys[pygame.K_UP]:
-            self.tank.rect.centery -= 5
+            self.spaceship.move(-5)
         if keys[pygame.K_DOWN]:
-            self.tank.rect.centery += 5
+            self.spaceship.move(5)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -43,7 +41,7 @@ class Game:
                 if event.key == pygame.K_ESCAPE:
                     self.run = False
                 if event.key == pygame.K_SPACE:
-                    bullet = Bullet(self.tank)
+                    bullet = Bullet(self.spaceship)
                     self.bullet_group.add(bullet)
                     self.all_sprites.add(bullet)
 
@@ -56,11 +54,11 @@ class Game:
         pygame.display.update()
 
 
-class Tank(pygame.sprite.Sprite):
+class Spaceship(pygame.sprite.Sprite):
 
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load("Schreibtisch/PYgame/Sprites/tank/tank.png")
+        self.image = pygame.image.load("Julien/PYgame/Sprites/spaceship/spaceship.png")
         self.org_image = self.image.copy()
 
     
@@ -87,21 +85,26 @@ class Tank(pygame.sprite.Sprite):
 
 class Bullet(pygame.sprite.Sprite):
 
-    def __init__(self, tank):
+    def __init__(self, spaceship):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load("Schreibtisch/PYgame/Sprites/bullet/bullet.png")
+        self.image = pygame.image.load("Julien/PYgame/Sprites/bullet/bullet.png")
         self.image = pygame.transform.scale(self.image, (16, 16))
         self.rect = self.image.get_rect()
-        self.rect.centerx = tank.rect.centerx + 3 # How much pixels from tank turret on x axis
-        self.rect.centery = tank.rect.centery - 25 # How much pixels from tank turret on y axis
+
+        # offset für den Bulletspawn (später ändern)
+        self.rect.centerx = spaceship.rect.centerx 
+        self.rect.centery = spaceship.rect.centery 
+
+        self.direction = pygame.Vector2(0, -1).rotate(-spaceship.angle)
+        self.pos = pygame.Vector2(self.rect.center)
 
     def update(self):
-        self.rect.y -= 10  # Move up 10 pixels per frame.
-
+        self.pos += self.direction * 10
+        self.rect.center = round(self.pos.x), round(self.pos.y)
 
 def main():
     pygame.init()
-    pygame.display.set_caption('Tank Game')
+    pygame.display.set_caption('Spaceship Game')
     clock = pygame.time.Clock()
     game = Game()
 
@@ -116,5 +119,3 @@ if __name__ == '__main__':
     main()
     pygame.quit()
     sys.exit()
-
-    #https://stackoverflow.com/questions/61106297/moving-forward-after-angle-change-pygame/61106823#61106823
