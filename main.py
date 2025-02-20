17c6a61a745255ebd87ce64aed8da530a3ad7141
geilo667 <76631611+geilo667@users.noreply.github.com>
2025-02-20 15:42:55 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import sys
 import pygame
 from game import Game
+from menu import Menu
 
 def main():
     pygame.init()
@@ -9,13 +10,18 @@ def main():
     pygame.display.set_caption('Spaceship Game')
     clock = pygame.time.Clock()
     game = Game()
+    menu = Menu()
 
-    while game.run:
-        game.handle_events()
-        game.update()
-        game.draw()
-        game.show_score(0,0)
-        clock.tick(60)
+    while menu.run:
+        menu.draw()
+        
+    if menu.start_game:
+        while game.run:
+            game.handle_events()
+            game.update()
+            game.draw()
+            game.show_score(0,0)
+            clock.tick(60)
 
 if __name__ == '__main__':
     main()
